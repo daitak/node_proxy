0c233fb4463c1658448d45f3e6ad36fc1b73661a
vagrant <vagrant@web1.(none)>
2014-07-08 11:06:56 +0000

    プロキシをモジュールとして別ファイルにした

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var redis = require('redis');
-var http = require('http');
-var url = require('url');
+var redis_proxy = require('./lib/redis_proxy.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var db = redis.createClient(6379,"192.168.56.11", {'return_buffers': true});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,59 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add Proxy
 //
-app.use(function(req, res, next){
-        db.exists(req.url, function(err, rexists){
-                if(rexists == true){
-                    db.lrange(req.url, 0, -1, function(err, list){
-                            res.writeHead(list[0], JSON.parse(list[1]));
-                            res.write(list[2]);
-                            res.end();
-                            console.log('Responsed cached contents!!!!!');
-                        });
-                }
-                else{
-                    var x = url.parse(req.url);
-                    var body = [];
-                    req.on('data', function(data){
-                            body.push(data);
-                        });
-
-                    var options = {host: x.hostname, port: x.port || 80, path: x.path,
-                        method: req.method, headers: req.headers };
-                    var server_req = http.request(options, function(server_res){
-                            res.writeHead(server_res.statusCode, server_res.headers);
-
-                            var bufs = [];
-                            bufs.totalLength = 0;
-                            server_res.on('data', function(chunk){
-                                    bufs.push(chunk);
-                                    bufs.totalLength += chunk.length;
-                                    res.write(chunk);
-                                });
-                            server_res.on('end', function(){
-                                    var data = Buffer.concat(bufs, bufs.totalLength);
-
-                                    if( server_res.statusCode == 200 ){
-                                        db.rpush(req.url, server_res.statusCode);
-                                        db.rpush(req.url, JSON.stringify(server_res.headers));
-                                        db.rpush(req.url, data);
-                                        db.expire(req.url, 100);
-                                        console.log('Set cache!!!!!');
-                                    }
-                                    res.end();
-                                });
-
-
-                        });
-                    if(body.length > 0){
-                        server_req.write(body.join(''));
-                    }
-                    server_req.end();
-                }
-            });
-    });
-
-
+app.use(redis_proxy.proxy({host: "192.168.56.11"}));
 
 app.use('/', routes);
 app.use('/users', users);
